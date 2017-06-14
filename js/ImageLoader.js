a9733266190c9d4f56e674fefa9a2203cb283e6b
Joao Pedro <jguedespresto@gmail.com>
2017-06-14 08:59:26 -0300

    Completar Arquivo ImageLoader

--- js/ImageLoader.js
+++ js/ImageLoader.js
@@ -1,3 +1,20 @@
 function ImageLoader(){
   this.images = {};
 }
+
+ImageLoader.prototype.load = function (key, url){
+  var img = new Image();
+  img.src = url;
+  this.images[key] = img;
+}
+
+ImageLoader.prototype.drawImage = function (ctx, key, x, y, size){
+  ctx.drawImage(this.images[key], x, y, size, size);
+}
+
+ImageLoader.prototype.drawImageTile = function (ctx, key, row, col, size, x, y){
+  ctx.drawImage(this.images[key],
+    col*size, row*size, size, size,
+    x, y, size, size
+  );
+}
