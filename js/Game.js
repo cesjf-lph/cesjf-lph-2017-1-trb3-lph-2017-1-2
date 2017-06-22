d5a30d2a3eb1ced0d16a4f1c266d90f0a67d2916
Joao Pedro <jguedespresto@gmail.com>
2017-06-22 15:34:09 -0300

    Movimentar personagens pelas torres

--- js/Game.js
+++ js/Game.js
@@ -140,6 +140,13 @@ function configuraControles() {
           }
           e.preventDefault();
           break;
+        case 65:
+          if (a.energia > 100 && auxiliar == 1){//Condiciona o lançamento a ter energia suficiente e o jogo estar em execução
+            mapa.criaPersonagem(3, 7);//(Linha, coluna) Se linha = 3 cria personagem na parte superior, se linha = 13 cria personagem na parte inferior, se coluna = 7 cria personagem de "a", se coluna = 30 cria personagem de "b"
+            a.energia = a.energia - 100;
+          }
+          e.preventDefault();
+          break;
         case 37:
           if (b.energia > 100 && auxiliar == 1){//Condiciona o lançamento a ter energia suficiente e o jogo estar em
             mapa.criaPersonagem(13, 30);//(Linha, coluna) Se linha = 3 cria personagem na parte superior, se linha = 13 cria personagem na parte inferior, se coluna = 7 cria personagem de "a", se coluna = 30 cria personagem de "b"
